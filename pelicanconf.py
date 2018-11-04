9084423b7ada3185079420797522eb76e0353472
Paul Gowder <paul.gowder@gmail.com>
2018-11-04 15:07:34 -0600

    add a lesson and tweak some settings

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Paul Gowder'
 SITENAME = 'Sociological Gobbledygook'
-SITEURL = ''
+SITEURL = 'https://sociologicalgobbledygook.com/'
 
 MARKUP = ('md', 'ipynb')
 PLUGIN_PATHS = ['./plugins']
@@ -12,11 +12,19 @@ PLUGINS = ["render_math", "ipynb.markup"]
 IGNORE_FILES = [".ipynb_checkpoints"]
 IPYNB_USE_METACELL = True
 
+
 PATH = 'content'
 
 TIMEZONE = 'America/Chicago'
 
 DEFAULT_LANG = 'en'
+
+# custom settings from reading docs
+USE_FOLDER_AS_CATEGORY = True
+DEFAULT_CATEGORY = 'Lessons'
+WITH_FUTURE_DATES = False
+NEWEST_FIRST_ARCHIVES = False
+ARTICLE_ORDER_BY = 'date'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -27,9 +35,8 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('University of Iowa College of Law', 'https://law.uiowa.edu/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+         ('Paul Gowder', 'https://gowder.io'),
+         ('Python Documentation', 'http://python.org/'),)
 
 # Social widget
 SOCIAL = (('Course Github', 'https://github.com/paultopia/quantitative-methods-for-lawyers/'),
