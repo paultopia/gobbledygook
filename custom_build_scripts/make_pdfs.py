25922d61347055e198916ab6944297bb0b0484ff
Paul Gowder <paul.gowder@gmail.com>
2019-02-07 12:20:32 -0600

    pdf supplementation for lessons works.

--- custom_build_scripts/make_pdfs.py
+++ custom_build_scripts/make_pdfs.py
@@ -12,7 +12,7 @@ def extract_title_ipynb(cell0):
 def add_link_to_pdf_ipynb(notebook_file):
     nb = nbformat.read(notebook_file, 4)
     pdffile = os.path.basename(notebook_file).split(".")[0] + ".pdf"
-    source = "\n<hr>\n[Download this lesson in PDF]({attach}/images/" + pdffile + ")\n<hr>"
+    source = "\n<hr>\n[Download this lesson in PDF]({attach}../images/" + pdffile + ")\n<hr>"
     if nb.cells[-1]["source"] != source:
         newcell = nbformat.from_dict({'cell_type': 'markdown', 'metadata': {}, 'source': source})
         nb.cells.append(newcell)
@@ -21,7 +21,7 @@ def add_link_to_pdf_ipynb(notebook_file):
 def remove_pdf_link_ipynb(notebook_file):
     nb = nbformat.read(notebook_file, 4)
     pdffile = os.path.basename(notebook_file).split(".")[0] + ".pdf"
-    source = "\n<hr>\n[Download this lesson in PDF]({attach}/images/" + pdffile + ")\n<hr>"
+    source = "\n<hr>\n[Download this lesson in PDF]({attach}../images/" + pdffile + ")\n<hr>"
     if nb.cells[-1]["source"] == source:
         nb.cells.pop()
         nbformat.write(nb, notebook_file)
@@ -33,7 +33,7 @@ def make_pdf_from_ipynb(notebook_file):
     firstcell = nb["cells"][0]
     nb["cells"][0] = extract_title_ipynb(nb["cells"][0])
     pdf, resources = exporter.from_notebook_node(nb)
-    outfile = "../content/images/" + notebook_file.split(".")[0] + ".pdf"
+    outfile = "content/images/" + os.path.basename(notebook_file).split(".")[0] + ".pdf"
     with open(outfile, "wb") as tp:
         tp.write(pdf)
     add_link_to_pdf_ipynb(notebook_file)
